133e22cc2421d562f54daa13abd41d96be2b442a
YaroslavTsvirkun <yaroslav.tsvirkun.1992@outlook.com>
2024-10-06 09:32:44 +0300

    Добавьте файлы проекта.

---
+++ Internet-Store-Engine.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Internet_Store_Engine.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
